eaf0e91326fe750fd21773378ad374726022335b
ClassicMC <classicmclive@gmail.com>
2022-11-19 10:50:09 -0500

    Create Newsbar.svelte

---
+++ src/elements/Newsbar.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+<style>
+	.item{
+		background-color: #0d161d;
+		text-align:center;
+	}
+</style>
+<main>
+<div class="w3-row">
+	<div class="w3-col item bc-r-25" style="margin: 1%; height: 100px; width: 220px;">
+	 <b>---------News---------</b><br/>Working on <b>Emulators</b><br/>Working on <b>Chrome web-apps</b>
+	</div>
+	<div class="w3-col item bc-r-25" style="margin: 1%; height: 100px; width: 220px;">
+		<b>-----Programmers-----</b><br/><spc class="bc-text-link" style="text-decoration: none!important;">
+			<a href="https://www.github.com/ClassicMC-Studios">ClassicMC Studios</a><br/>
+			<a href="https://www.github.com/CACcodes">CACcodes</a></spc>
+	</div>
+</div>
+</main>

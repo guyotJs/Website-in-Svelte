4c62b2389fc749531256a787d5fa8de9822cbfbc
ClassicMC <classicmclive@gmail.com>
2022-11-19 10:49:39 -0500

    Create Grid.svelte

---
+++ src/elements/Grid.svelte
@@ -0,0 +1,59 @@
+<script>
+
+</script>
+<style>
+	.item{
+		background-color: #0d161d;
+		text-align:center;
+	}
+	.bleux{transition: 0.3s;}
+	.bleux:hover{color: skyblue;}
+	blue{color:blue;}
+	.hv{
+		background-color: #0d161d;
+		transition: 0.3s; 
+	}
+	.hv:hover{background-color: #182835;} 
+</style>
+<main>
+<!-- Layer 1	 -->
+<div class="w3-row">
+<!-- Item 1 -->
+	<div class="w3-col item w3-margin bc-r-25 hv" style="width:220px; height:220px;">
+		<p>
+			Welcome to<br/><em>Guyot</em><br/>The plateau of the<br/><b>Chromebook</b><br/>
+			<img src="https://classicmc-studios.github.io/guyot/comp.png" alt="computer"/>
+		</p>
+	</div>
+<!-- Item 2 -->
+	<div class="item w3-col w3-margin bc-r-25 hv" style="width:220px; height:220px;">
+	<h1><b>Github</b></h1><br/><a href="https://github.com/guyotJs"><i class="material-icons"><blue class="bleux">link</blue></i></a><br/><img src="./duck.png" alt="" height="0" width="200"/><br/>This website was made in<br/>Svelte!
+	</div>
+</div>
+<!-- Layer 2 -->
+<div class="w3-row">
+<!-- Item 1	 -->
+	<div class="w3-col item w3-margin bc-r-25 hv" style="width:220px; height:220px;">
+		<h1><blue><b>v0.5.5</b></blue></h1>
+		<p>
+			<b>Changelog</b>
+		</p>
+		<p>
+			- Added "Upcoming"<br/>
+			- Added links to github<br/>
+			- Div grid scaling in w3.css
+		</p>
+	</div>
+<!-- Item 2	 -->
+	<div class="w3-col item w3-margin bc-r-25 hv" style="width:220px; height:220px;">
+		<h1><b>Info</b></h1><p>This website is <em>open source</em>!</p><p>Help us <a href="https://github.com/guyotJs/guyotJs.github.io">improve</a></p><br/>
+	</div>
+<!-- Upcoming -->
+	<div class="w3-col item w3-margin bc-r-25 hv" style="width:220px; height:220px;">
+		<h1>Upcoming</h1>
+		<p><blue>Super Mario Land</blue></p>
+		<p><blue>Super Mario Kart(2ply*)</blue></p>
+		<p><blue>Final Fantasy VI</blue></p>
+	</div>
+</div>
+</main>

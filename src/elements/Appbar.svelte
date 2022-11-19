ebb942d4aae822e4346c01ea6b7449bfe2fb7cd6
ClassicMC <classicmclive@gmail.com>
2022-11-19 10:49:19 -0500

    Create Appbar.svelte

---
+++ src/elements/Appbar.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+<style>
+	a{text-decoration: none!important;}
+  .bc-d_b{background-color: #0a1217;}
+</style>
+<main>
+<!--  <div class="gamebar bc-goldenrod bc-r-25" style="margin: 1%; height: 100px;">
+		_Div class="gamebar"
+	</div>
+	<div class="appbar bc-sky bc-r-25" style="margin: 1%; height: 100px;">
+		_Div class="appbar"
+	</div> -->
+		<div class="bc-d_b bc-r-10 " style="margin:1%;">
+		<i class="material-icons">videogame_asset</i><br/>
+		<a href="https://classicmc-studios.github.io/guyot/games/spyro.html" class="bc-text-link ">Spyro Season of Flame__</a> <a href="https://classicmc-studios.github.io/guyot/games/yoshi.html" class="bc-text-link ">Yoshi's Island</a><br/>
+		<a href="https://classicmc-studios.github.io/guyot/games/ffv.html" class="bc-text-link ">Final Fantasy V__</a><a href="https://classicmc-studios.github.io/guyot/games/yo.html" class="bc-text-link "> Yoshi's Island ROM hack</a><br/>
+		<a href="https://classicmc-studios.github.io/guyot/games/emerald.html" class="bc-text-link ">Pokémon Emerald</a><br/>
+		<a href="https://classicmc-studios.github.io/guyot/games/leafgreen.html" class="bc-text-link ">Pokémon Leaf Green</a><br/>
+	</div>
+		<div class="bc-r-10 bc-d_b" style="margin:1%; height: 100px;">
+        <i class="material-icons">laptop_chromebook</i><br/>
+        <a href="https://classicmc-studios.github.io/pyt" class="bc-text-link">Pythagorean Therom Calculator</a><br/>
+        <a href="./calc/index.html" class="bc-text-link">One operation calculator</a>
+    </div>
+</main>
